5c433a81fffd0359dd32a5bf1d9823b21e62cdca
Steve Grossi <me@stevegrossi.com>
2016-12-17 11:31:30 -0500

    Mary had a little lamb…

--- lib/sweet_beats.ex
+++ lib/sweet_beats.ex
@@ -1,2 +1,24 @@
 defmodule SweetBeats do
+
+  @tempo 250
+
+  @doc ~S"""
+  Splits a string of notes and plays them. Spaces denote rests.
+
+      iex> SweetBeats.song("BAGABBB AAA BDD BAGABBBBAABAG")
+
+  """
+  def song(notes) do
+    notes
+    |> String.graphemes
+    |> Enum.each(fn(note) ->
+         spawn fn -> note(note) end
+         :timer.sleep(@tempo)
+       end)
+  end
+
+  defp note(" "), do: nil
+  defp note(note) do
+    System.cmd("play", ["-qn", "synth", "1", "pluck", note])
+  end
 end
