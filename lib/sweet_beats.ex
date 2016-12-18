7cc51493cd25393d5b349e00d3617fa4a8a856d4
Steve Grossi <me@stevegrossi.com>
2016-12-18 09:31:07 -0500

    Let’s add some beats

--- lib/sweet_beats.ex
+++ lib/sweet_beats.ex
@@ -17,8 +17,29 @@ defmodule SweetBeats do
        end)
   end
 
+  def beats(files) do
+    files
+    |> Enum.each(fn(sample) ->
+         spawn fn -> play_sample(sample) end
+         :timer.sleep(@tempo)
+       end)
+    beats(files)
+  end
+
+  def composition do
+    spawn fn -> song("BAGABBB AAA BDD BAGABBBBAABAG") end
+    spawn fn -> beats(~w(snare1 kick3 kick3 kick3)) end
+    spawn fn -> beats(["kick1", " " ," ", "clap"]) end
+    spawn fn -> beats([" ", " " ,"tom2", "tom1"]) end
+  end
+
   defp note(" "), do: nil
   defp note(note) do
     System.cmd("play", ["-qn", "synth", "1", "pluck", note])
+  end
+
+  defp play_sample(" "), do: nil
+  defp play_sample(filename) do
+    System.cmd("play", ["-q", "media/#{filename}.wav"])
   end
 end
